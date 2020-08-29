70413b06401fc5b05473c47ab1a04de829e1f097
Вадим <vraspopin@inbox.ru>
2020-08-29 23:34:10 +0300

    Task 1

---
+++ src/JavaCore/Base/Lesson4/XOGame.java
@@ -0,0 +1,148 @@
+package JavaCore.Base.Lesson4;
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+public class XOGame {
+    static final int SIZE = 3;
+//    static final int DOT_TO_WIN = 3;
+
+    static final char DOT_X = 'X';
+    static final char DOT_O = 'O';
+    static final char DOT_EMPTY = '.';
+
+    static char[][] map;
+
+    static Scanner sc = new Scanner(System.in);
+    static Random random = new Random();
+
+    public static void main(String[] args) {
+        initMap();
+        printMap();
+
+        while (true) {
+            humanTurn();
+            printMap();
+            if (checkWin(DOT_X)) {
+                System.out.println("Вы победили! Поздравляем!");
+                break;
+            }
+            if (isFull()) {
+                System.out.println("Ничья");
+                break;
+            }
+
+            aiTurn();
+            printMap();
+            if (checkWin(DOT_O)) {
+                System.out.println("Компьютер победил.");
+                break;
+            }
+            if (isFull()) {
+                System.out.println("Ничья");
+                break;
+            }
+        }
+
+    }
+
+    public static void initMap() {
+        map = new char[SIZE][SIZE];
+        for (int i = 0; i < map.length; i++) {
+            Arrays.fill(map[i], DOT_EMPTY);
+        }
+    }
+
+    public static void printMap() {
+        System.out.print("  ");
+        for (int i = 1; i <= map.length; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+        for (int i = 0; i < map.length; i++) {
+            System.out.print(i + 1 + " ");
+            for (int j = 0; j < map[i].length; j++) {
+                System.out.printf("%c ", map[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void humanTurn() {
+        int x;
+        int y;
+
+        do {
+            System.out.println("input X Y");
+            x = sc.nextInt() - 1;
+            y = sc.nextInt() - 1;
+        } while (!isCellValid(y, x));
+
+        map[y][x] = DOT_X;
+    }
+
+    public static boolean isCellValid(int y, int x) {
+        if (y < 0 || x < 0 || y >= SIZE || x >= SIZE) {
+            return false;
+        }
+        return map[y][x] == DOT_EMPTY;
+    }
+
+    public static void aiTurn() {
+        int x;
+        int y;
+
+        do {
+            x = random.nextInt(SIZE);
+            y = random.nextInt(SIZE);
+        } while (!isCellValid(y, x));
+
+        map[y][x] = DOT_O;
+    }
+
+    public static boolean isFull() {
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                if (map[i][j] == DOT_EMPTY) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public static boolean checkWin(char c) {
+        for (int i = 0; i < SIZE; i++) {
+            boolean horizontalWin = true;
+            boolean verticalWin = true;
+            for (int j = 0; j < SIZE; j++) {
+                if (map[i][j] != c) {
+                    horizontalWin = false;
+                }
+                if (map[j][i] != c) {
+                    verticalWin = false;
+                }
+            }
+            if (horizontalWin || verticalWin) {
+                return true;
+            }
+        }
+        boolean diagonal1 = true;
+        boolean diagonal2 = true;
+        for (int i = 0; i < SIZE; i++) {
+            if (map[i][i] != c) {
+                diagonal1 = false;
+            }
+            if (map[i][SIZE - i - 1] != c) {
+                diagonal2 = false;
+            }
+        }
+        if (diagonal1 || diagonal2) {
+            return true;
+        }
+
+        return false;
+    }
+
+}
